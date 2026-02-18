4900dc6392dbf2bee43adb26c556d8101f150299
Neve-06 <6255234@rts-365.dk>
2026-02-18 14:25:19 +0100

    The login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/Loginform/Loginform"
+
+export default function Loginpage() {
+
+    return (
+        <>
+        <main>
+        <LoginForm/>
+        </main>
+        </>
+    )
+}
