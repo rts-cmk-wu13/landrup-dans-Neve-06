e4211f809d2159566abddc3cdce6cdfb09e91f56
Neve-06 <6255234@rts-365.dk>
2026-02-18 13:04:21 +0100

    Added a hero component with the hero image.

---
+++ src/app/components/Loginform/Hero/hero.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+
+export default function Hero() {
+
+    return (
+        <>
+        <div className="hero">
+        <Image 
+        src="/heroimg.jpg" 
+        width={330}
+        height={300}
+        alt="hero">
+        </Image>
+        </div>
+        </>
+    )
+}
