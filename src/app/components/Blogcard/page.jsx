e2af995c0f43582e6cdfaf9b7e4403414a5da98b
Neve-06 <6255234@rts-365.dk>
2026-02-19 15:44:16 +0100

    added a figure tag around the img tags

--- src/app/components/Blogcard/page.jsx
+++ src/app/components/Blogcard/page.jsx
@@ -2,15 +2,17 @@ import Link from "next/link"
 
 export default function BlogCard({ activity }) {
 
-    const formattedDate = Intl.DateTimeFormat("da-DK", {dateStyle: "long"}).format(new Date(activity.createdAt))
+    // const formattedDate = Intl.DateTimeFormat("da-DK", {dateStyle: "long"}).format(new Date(activity.createdAt))
     return (
         <>
-        <Link href={`/blog/${activity.id}`} aria-labelledby={"blog-card-" + activity.id}>
+        <Link href={`/activities/${activity.id}`} aria-labelledby={"activities" + activity.id}>
             <article>
-                <h2 id={"blog-card-" + activity.id}>{activity.name}</h2>
-                <p>{activity.author}</p>
-                <p>{formattedDate}</p>
-                <p>{activity.content}</p>
+                <h2 id={"activities" + activity.id}>{activity.name}</h2>
+                {/* <p>{formattedDate}</p> */}
+                <figure className="logo">
+                <img src={activity.asset.url} alt={activity.name}/>
+                </figure>
+                <p>{activity.maxParticipants}</p>
             </article>
         </Link>
         </>
