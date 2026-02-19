9faaebb059d762cf325a001b870d80e29037c2f0
Neve-06 <6255234@rts-365.dk>
2026-02-19 15:43:52 +0100

    created a dal file with fetches

---
+++ src/app/lib/dal.js
@@ -0,0 +1,23 @@
+"use server"
+
+
+
+
+export async function getAllActivities(){
+    const response = await fetch(`http://localhost:4000/api/v1/activities/`)
+
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
+    }
+    return await response.json()
+
+}
+export async function getActivityById(id) {
+    const response = await fetch(`http://localhost:4000/api/v1/activities/${id}`)
+
+    if (!response.ok) {
+        throw new Error({ message: "something went wrong" })
+    }
+    return await response.json()
+
+}
