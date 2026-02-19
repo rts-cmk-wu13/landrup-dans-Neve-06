a366110dfd47d765d9cba8acf80172771ba4f972
Neve-06 <6255234@rts-365.dk>
2026-02-19 11:13:14 +0100

    imported Link to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import Hero from "@/app/components/Loginform/Hero/hero"
-
+import Hero from "@/app/components/Hero/hero"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="root">
       <main className="">
         <Hero/>
+        <Link className="login__button"href="/login">Login</Link>
       </main>
     </div>
   );
