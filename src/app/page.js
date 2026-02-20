6a5ae9a2622abf2fc118c2babc6eac94037561bf
Neve-06 <6255234@rts-365.dk>
2026-02-20 15:23:01 +0100

    Imported testimonials to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Hero from "@/app/components/Hero/hero"
 import Link from "next/link";
 import Groups from "@/app/components/Groups/groups";
+import Testimonials from "@/app/components/Testimonials/Testimonials";
 
 export default function Home() {
   return (
-    <div className="root">
       <main className="">
         <Hero/>
         <Link className="login__button"href="/login">Login</Link>
         <Groups/>
+        <Testimonials/>
       </main>
-    </div>
   );
 }
