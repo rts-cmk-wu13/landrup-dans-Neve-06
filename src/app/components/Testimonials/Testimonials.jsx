a9ca7bbde05b9cea57fb3f79620944a7d9f50b52
Neve-06 <6255234@rts-365.dk>
2026-02-20 13:57:15 +0100

    template for the carousel. It will get to work on that one

---
+++ src/app/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+
+export default function Testimonals() {
+  const [emblaRef, emblaApi] = useEmblaCarousel( { loop: true } )
+
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          <div className="embla__slide">Slide 1</div>
+          <div className="embla__slide">Slide 2</div>
+          <div className="embla__slide">Slide 3</div>
+        </div>
+      </div>
+
+      <button className="embla__prev">Scroll to prev</button>
+      <button className="embla__next">Scroll to next</button>
+    </div>
+  )
+}
