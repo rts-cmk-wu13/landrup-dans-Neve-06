5af9d0191d9d07a298ad6fba235db93bfb352441
Neve-06 <6255234@rts-365.dk>
2026-02-20 13:48:50 +0100

    fixed the proxy, by putting it in the correct folder

--- src/proxy.js
+++ src/proxy.js
@@ -3,6 +3,8 @@
 import { NextResponse } from "next/server";
 
 export function proxy(request) {
+    console.log(request.cookies.has("authToken"));
+    
     if(!request.cookies.has("authToken")){
         return NextResponse.redirect(new URL ('/no-access', request.url));
     }}
