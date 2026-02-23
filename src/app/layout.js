187e4515cb348d4f1a9016eb22a5fd9cbf1ca4c1
Neve-06 <6255234@rts-365.dk>
2026-02-23 14:56:20 +0100

    finally added the footer to the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} root`}>
         <Header/>
           {children}
-        {/* <Footer/> */}
+        <Footer/>
       </body>
     </html>
   );
