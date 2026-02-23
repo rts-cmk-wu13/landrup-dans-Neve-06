c3ad4288883d441c1f0ce3c0e1b741f8a91e856f
Neve-06 <6255234@rts-365.dk>
2026-02-23 15:01:31 +0100

    translated from english to danish

--- src/app/no-access/page.jsx
+++ src/app/no-access/page.jsx
@@ -4,9 +4,9 @@ export default function NoAccessPage() {
     return (
         <main>
             <div className="no-columns">
-            <h1>No Access</h1>
-            <p>You have to log in!</p>
-            <Link href="/login">Go to Login Page</Link>
+            <h1>Ingen Adgang!</h1>
+            <p>Du skal logge ind!</p>
+            <Link href="/login">Tilbage til Login siden</Link>
             </div>
         </main>
     )
