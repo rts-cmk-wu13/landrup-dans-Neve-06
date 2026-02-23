f0a25a83ef6976c3599d68d7f7ef6fdfee8a9f7f
Neve-06 <6255234@rts-365.dk>
2026-02-23 14:40:43 +0100

    Made it so when you are on the home page, HomeFooter shows up, but if you are on ANY other page, FixedFooter shows up via usePathname

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 "use client"
+import Image from "next/image"
 import { usePathname } from "next/navigation"
+import Link from "next/link"
 
 export default function Footer() {
     const pathname = usePathname()
 
     if (pathname === "/") return <HomeFooter />
+
     return (
         <>
             <FixedFooter />
@@ -15,7 +18,14 @@ export default function Footer() {
 export function HomeFooter() {
     return (
         <>
-            test1
+            <footer>
+                <figure className="logo">
+                    <Image src="/logo.svg" width={75} height={25} alt="logo"></Image>
+                </figure>
+                <figure className="logo">
+                    <Image src="/title.svg" width={250} height={100} alt="logo__title"></Image>
+                </figure>
+            </footer>
         </>
     )
 }
@@ -24,7 +34,13 @@ export function FixedFooter() {
     return (
 
         <>
-            test2
+            <footer className="fixed__footer">
+                <ul>
+                    <li><Link href="/"></Link></li>
+                    <li><Link href="/activities"></Link></li>
+                    <li><Link href="/profile"></Link></li>
+                </ul>
+            </footer>
         </>
     )
 }
