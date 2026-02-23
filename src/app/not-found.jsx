de1b9ce1618a779f10612f43d45575ece08acccd
Neve-06 <6255234@rts-365.dk>
2026-02-23 13:09:32 +0100

    added "no-columns for not found"

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,8 +2,11 @@ export default function NotFound() {
 
     return (
         <main>
+            <section className="columns">
+
             <h1>Page not found</h1>
             <p>The page you are looking for does not exist.</p>
+            </section>
         </main>
     )
 }
