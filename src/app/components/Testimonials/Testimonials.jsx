1479fd04cc21d751fb0f044a818a47a0dac2754d
Neve-06 <6255234@rts-365.dk>
2026-02-23 13:41:17 +0100

    installed react icons into the testimonials, as well as some styling

--- src/app/components/Testimonials/Testimonials.jsx
+++ src/app/components/Testimonials/Testimonials.jsx
@@ -2,6 +2,8 @@
 import React from "react"
 import useEmblaCarousel from "embla-carousel-react"
 import Autoplay from "embla-carousel-autoplay"
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
 
 export default function Testimonials({ testimonials = [] }) {
     // Initialize Embla carousel and keep it looping infinitely.
@@ -15,26 +17,32 @@ export default function Testimonials({ testimonials = [] }) {
     const scrollNext = () => emblaApi?.scrollNext()
 
     return (
-        <div className="embla">
-            {/* Embla controls this viewport element via emblaRef. */}
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
-                    {/* Render each testimonial as one slide in the carousel. */}
-                    {testimonials.map((testimonial) => (
-                        <article className="embla__slide" key={testimonial.id}>
-                            <p>{testimonial.name}</p>
-                            <p>{testimonial.content}</p>
-                        </article>
-                    ))}
+        <>
+            <div className="embla">
+                {/* Embla controls this viewport element via emblaRef. */}
+                <h2 className="embla__container-title">Det siger vores kunder om os</h2>
+                <div className="embla__viewport" ref={emblaRef}>
+                    <div className="embla__container">
+
+                        {/* Render each testimonial as one slide in the carousel. */}
+                        {testimonials.map((testimonial) => (
+                            <article className="embla__slide" key={testimonial.id}>
+                                <p>{testimonial.content}</p>
+                                <p>{testimonial.name}</p>
+                                <p>{testimonial.occupation}</p>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+                <div className="embla__button-container">
+                    <button className="embla__button">
+                        <IoIosArrowBack style={{ color: "#ffffff" }} onClick={scrollPrev} />
+                    </button>
+                    <button className="embla__button">
+                        <IoIosArrowForward style={{ color: "#ffffff" }} onClick={scrollNext} />
+                    </button>
                 </div>
             </div>
-
-            <button className="embla__prev" onClick={scrollPrev}>
-                Scroll to prev
-            </button>
-            <button className="embla__next" onClick={scrollNext}>
-                Scroll to next
-            </button>
-        </div>
+        </>
     )
 }
