c629acb350782574eb77913e1664cded6959e0c6
Neve-06 <6255234@rts-365.dk>
2026-02-24 15:38:13 +0100

    added an icon for the return button on the detail page

--- src/app/activities/[id]/page.jsx
+++ src/app/activities/[id]/page.jsx
@@ -3,6 +3,7 @@ import { getActivityById } from "../../lib/dal";
 import Link from "next/link";
 // import CommentSection from "@/app/components/CommentSection/page.jsx";
 import { notFound } from "next/navigation";
+import { IoIosArrowBack } from "react-icons/io";
 export default async function ActivityDetailPage({ params }) {
 
 
@@ -14,7 +15,7 @@ export default async function ActivityDetailPage({ params }) {
     
     return (
         <main>
-            <Link href="/activities">Return to activities</Link>
+            <Link href="/activities"><IoIosArrowBack/></Link>
             <article>
                 <figure className="activity__image-figure">
                 <img src={activity.asset.url} alt={activity.name} />
