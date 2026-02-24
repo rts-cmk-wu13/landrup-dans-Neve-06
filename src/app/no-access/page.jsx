a8544a801457e31fc2b03aaec74a74cc3735e276
Neve-06 <6255234@rts-365.dk>
2026-02-24 15:38:43 +0100

    made styling and improvements to no access

--- src/app/no-access/page.jsx
+++ src/app/no-access/page.jsx
@@ -3,10 +3,14 @@ import Link from "next/link"
 export default function NoAccessPage() {
     return (
         <main>
-            <div className="no-columns">
+            <div className="no-columns notfound">
             <h1>Ingen Adgang!</h1>
             <p>Du skal logge ind!</p>
-            <Link href="/login">Tilbage til Login siden</Link>
+            <Link href="/login">
+            <button className="login__button">
+            Tilbage til Login siden
+            </button>
+            </Link>
             </div>
         </main>
     )
