cff8f262ad20b1c27b4dbb7d2ffbf0910d6c3a9b
Neve-06 <6255234@rts-365.dk>
2026-02-24 15:13:37 +0100

    put the variables inside the function, otherwise it didn't work properly

--- src/app/lib/dal.js
+++ src/app/lib/dal.js
@@ -1,31 +1,29 @@
 "use server"
 import { cookies } from "next/headers"
-const cookieStore = await cookies()
-const authToken = cookieStore.get("authToken")
 
 export async function getAllActivities(){
     const response = await fetch(`http://localhost:4000/api/v1/activities/`)
-
+    
     if (!response.ok) {
         throw new Error({ message: "something went wrong" })
     }
     return await response.json()
     
-
+    
 }
 export async function getActivityById(id) {
     const response = await fetch(`http://localhost:4000/api/v1/activities/${id}`)
-
+    
     if (!response.ok) {
         throw new Error({ message: "something went wrong" })
     }
     return await response.json()
-
+    
 }
 
 export async function getTestimonials() {
     const response = await fetch(`http://localhost:4000/api/v1/testimonials/`)
-
+    
     if (!response.ok) {
         throw new Error({ message: "something went wrong" })
     }
@@ -33,6 +31,8 @@ export async function getTestimonials() {
 }
 
 export async function getUsersById(userId) {
+    const cookieStore = await cookies()
+    const authToken = cookieStore.get("authToken")
     const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`,
         {
             method: "GET",
