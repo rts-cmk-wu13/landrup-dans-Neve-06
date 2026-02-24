6a3c8396e03dedd06f474e54fe40212526055d24
Neve-06 <6255234@rts-365.dk>
2026-02-24 15:31:27 +0100

    made improvements to notfound.jsx

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,18 @@
+import Link from "next/link"
+
 export default function NotFound() {
 
     return (
         <main>
-            <section className="columns">
+            <section className="columns notfound">
 
-            <h1>Page not found</h1>
-            <p>The page you are looking for does not exist.</p>
+                <h1>Hov! Hvad laver du her?</h1>
+                <p>Denne side findes ikke</p>
+                <Link href="/">
+                    <button className="login__button">
+                        Tilbage til forsiden
+                    </button>
+                </Link>
             </section>
         </main>
     )
