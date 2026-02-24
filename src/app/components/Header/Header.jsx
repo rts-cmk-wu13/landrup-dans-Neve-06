d2e9502d10412bcabe5ad6d97861c6b8fa72a9dc
Neve-06 <6255234@rts-365.dk>
2026-02-24 14:45:44 +0100

    made a header component in the same style as the footer, nothing shows up in the normal header but when on the profile page, you get a header

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,10 +1,25 @@
-import { navigate } from "next/navigation"
+"use client"
+import { usePathname } from "next/navigation"
 
 export default function Header() {
+
+    const pathname = usePathname()
+    
+    if (pathname === "/profile") return <ProfileHeader/>
 
     return (
         <>
         
+        </>
+    )
+}
+
+export function ProfileHeader() {
+    return (
+        <>
+            <header className="full-width">
+                <h1 className="">Profil</h1>
+            </header>
         </>
     )
 }
